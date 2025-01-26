14aa89dc575dacb5adc8ecd3c4d7a51f4658f2bc
ai3izu <sebcioo003@gmail.com>
2025-01-26 17:11:54 +0100

    code refractor

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-function speak(){
+function speak() {
     const text = document.getElementById('text').value;
-
-    if(text.length === 0){
+    if (text.length === 0) {
         alert('Wprowadz tekst.');
         return;
     }
 
-    if('speechSynthesis' in window){
+    if ('speechSynthesis' in window) {
         const speech = new SpeechSynthesisUtterance(text);
-        speech.lang = 'pl-PL';
 
+        speech.lang = 'pl-PL';
         speech.rate = 1;
         speech.pitch = 1;
-
         window.speechSynthesis.speak(speech);
-    }else{
-        alert('failed to load speech API')
     }
+    else alert('failed to load speech API');
 }
 
 function stop() {
-
-    
-
     if ('speechSynthesis' in window) {
-        if(window.speechSynthesis.speaking){
+        if (window.speechSynthesis.speaking) {
             window.speechSynthesis.cancel();
-        }else{
-            alert("Nic nie jest obecnie odtwarzane.");
         }
-    } else {
-        alert("failed to load speech API");
+        else alert("Nic nie jest obecnie odtwarzane.");
     }
+    else alert("failed to load speech API");
 }
